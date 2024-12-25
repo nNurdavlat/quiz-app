c277f41ef8565ba8da2f9a84b580688d6338ecb5
Your Name <you@example.com>
2024-12-25 20:50:42 +0500

    PATCH: Debugging

--- src/Models/User.php
+++ src/Models/User.php
@@ -3,19 +3,28 @@
 namespace App\Models;
 
 use App\Models\DB;
+
+use App\Traits\HasApiTokens;
 use PDO;
+
+
+
 class User extends DB  // User ichida DB property lari bor meros olganimiz uchun
 {
+    use HasApiTokens;
     public function create(string $fullName, string $email, string $password): bool  // REGISTER
     {
         $query = "INSERT INTO users (full_name, email, password, updated_at, created_at) VALUES (:fullName, :email, :password, NOW(), NOW())";
-        return $stmt = $this->conn
+        $stmt = $this->conn
             ->prepare($query)
             ->execute([
             ':fullName' => $fullName,
             ':email' => $email,
             ':password' => password_hash($password, PASSWORD_DEFAULT)
         ]);
+        $userId = $this->conn->lastInsertId();
+        $this-> createApiToken($userId);
+        return $userId;
     }
 
     public function getUser(string $email, string $password): bool  // LOGIN
@@ -27,7 +36,7 @@ class User extends DB  // User ichida DB property lari bor meros olganimiz uchun
             ':email' => $email
         ]);
         $user = $stmt->fetch();
-        if (password_verify($password, $user->password)) // User kiritgan parol bilan hashlangan parol to'g'ri bo'lsa TRUE
+        if ($user && password_verify($password, $user->password)) // User kiritgan parol bilan hashlangan parol to'g'ri bo'lsa TRUE
         {
             return true;
         }
---
+++ src/Traits/HasApiTokens.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Traits;
+
+use Random\RandomException;
+
+trait HasApiTokens
+{
+    protected string $api_token;
+    protected string $duration;
+
+    public function createApiToken(int $userId): string
+    {
+        $query = "INSERT INTO user_api_tokens (user_id, token, expires_at) 
+                    VALUES (:userId, :token, :expires_at)";
+
+        $this->api_token = bin2hex(random_bytes(40)); // 40 ta symbol aralashtirvoradi
+        $this->duration = date("Y-m-d H:i:s", strtotime("+" . $_ENV['API_TOKEN_EXPIRE_DAY'] . "day"));
+
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute([
+            "userId" => $userId,
+            "token" => $this->api_token,
+            "expires_at" => $this->duration
+        ]);
+        return $this->api_token;
+    }
+}
