30556f5bc2f23856f66a49e6d92f05a2adc119bf
Your Name <you@example.com>
2025-01-06 15:26:43 +0500

    MINOR: Add Tailwind package

--- resources/views/home.php
+++ resources/views/home.php
@@ -5,16 +5,20 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="./css/tailwind.css">
+    <link rel="stylesheet" href="./css/style.css">
+
     <title>Quiz App</title>
     <style>
         h1{
             text-align: center;
         }
     </style>
-    <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
+    <h1 class="text-green underline">Hello World</h1>
     <h1 style="color: red">Hello World</h1>
-    <h1 class="text-green">Hello World</h1>
+    <h1>Hello World</h1>
 </body>
 </html>
