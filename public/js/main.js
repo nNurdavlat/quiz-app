90a9cfe213ef42d9862c4c454b5dffda656ffe75
Your Name <you@example.com>
2025-01-08 15:22:37 +0500

    PATCH: Add javascript in code

---
+++ public/js/main.js
@@ -0,0 +1,10 @@
+function test(){
+    let errorMessage = document.getElementById('forTest'),
+        email = document.getElementById('email'),
+        password = document.getElementById('password');
+
+    if(email.value === '' || password.value === ''){
+        errorMessage.innerHTML = 'Please enter email address and password';
+        errorMessage.style.color = 'red';
+    }
+}
