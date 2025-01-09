77953734774872fe80f37a44d3de7ecf75718601
Your Name <you@example.com>
2025-01-09 13:15:44 +0500

    PATCH: Add register and login functions in main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,25 +1,52 @@
-function test(){
-    let errorMessage = document.getElementById('forTest'),
-        email = document.getElementById('email'),
-        password = document.getElementById('password');
+function login(){
+    let form = document.getElementById('form'),
+        formDate = new FormData(form);
 
-    if(email.value === '' || password.value === ''){
-        errorMessage.innerHTML = 'Please enter email address and password';
-        errorMessage.style.color = 'red';
-    }
+    fetch('http://localhost:8080/api/login', {
+        method: 'POST',
+        body: formDate,
+    })
+
+        .then(function(response){
+
+        if(response.ok){
+            return response.json();
+        }
+
+        return Promise.reject(response);
+    })
+
+        .then(function(data){
+            localStorage.setItem('token', data.token);
+            console.log(localStorage.getItem('token'));
+        })
+        .catch(function(error){
+            console.log(error);
+        });
 }
 
+function register(){
+    let form = document.getElementById('form'), // Auth/register.php  form ni idsidagi nom
+        formDate = new FormData(form);
 
+    fetch('http://localhost:8080/api/register', {
+        method: 'POST',
+        body: formDate,
+    })
 
+    .then(function(response){
 
+        if(response.ok){
+            return response.json();
+        }
 
+        return Promise.reject(response);
+    })
 
-
-
-
-
-
-
-
-+
-
+    .then(function(data){
+        console.log(data);
+    })
+    .catch(function(error){
+        console.log(error);
+    });
+}
--- resources/views/auth/login.php
+++ resources/views/auth/login.php
@@ -19,7 +19,7 @@
                 </a>
             </p>
         </div>
-        <form class="mt-8 space-y-6" action="#" method="POST">
+        <form id="form" class="mt-8 space-y-6" action="#" method="POST">
             <div class="rounded-md shadow-sm -space-y-px">
                 <div>
                     <label for="email" class="sr-only">Email address</label>
@@ -45,7 +45,7 @@
             </div>
 
             <div>
-                <button type="button" onclick="test()"
+                <button type="button" onclick="login()"
                         class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                     Sign in
                 </button>
