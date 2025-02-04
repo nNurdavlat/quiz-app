e990b081e71a582301e8e260f57a7f5c75614e90
Your Name <you@example.com>
2025-02-04 14:50:29 +0500

    Update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -1,11 +1,16 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('error_reporting', E_ALL);
+
 
 require 'vendor/autoload.php';
 
 date_default_timezone_set('Asia/Tashkent');
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
+
+if ($_ENV['APP_DEBUG']){
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+}
 
 require 'helpers.php';
