05162ada6a247552aea064daaf22dc68c907fcdc
Your Name <you@example.com>
2025-02-04 19:36:05 +0500

    PATCH

--- public/js/dashboard/getUserInfo.js
+++ public/js/dashboard/getUserInfo.js
@@ -1,12 +1,15 @@
 async function user()
 {
-    const {default: apiFetch} = await import('../utils/apiFetch.js');
+    const {default: apiFetch} = await import('/public/js/utils/apiFetch.js');
     await apiFetch('/users/getInfo', {method: 'GET'})
         .then((user) => {
             document.getElementById('userName').innerText = user.data.full_name;
         })
         .catch((error) =>
         {
+            if (error.status == 401) {
+                window.location.href = '/login';
+            }
         });
 }
 user();
